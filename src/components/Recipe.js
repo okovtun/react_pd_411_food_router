75b2cad41ba1c8b12da323188bc3ad069fc67033
User <User@DESKTOP-QHG18FL>
2026-03-11 16:11:03 +0300

    Food engine DONE

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -20,6 +20,44 @@ function Recipe()
                 <div>Category: <h3>{recipe.strCategory}</h3></div>
                 {recipe.strArea ? <div>Area: <h4>{recipe.strArea}</h4></div> : null }
                 <p>{recipe.strInstructions}</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Ingredient</th>
+                            <th>Measure</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Object.keys(recipe).map
+                            (
+                                key =>
+                                {
+                                    if(key.includes('Ingredient') && recipe[key])
+                                    {
+                                        return(
+                                            <tr key={key}>
+                                                <td>{recipe[key]}</td>
+                                                <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                                            </tr>
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    </tbody>
+                </table>
+                {
+                    !(recipe.strYoutube) ? null :
+                    (
+                        <div>
+                            <h4>Video recipe</h4>
+                            <div className='video-wrap'>
+                                <iframe title={id} src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}`} allowFullScreen />
+                            </div>
+                        </div>
+                    ) 
+                }
             </div>
             <button className='btn' onClick={goBack}>Go back</button>
         </div>
